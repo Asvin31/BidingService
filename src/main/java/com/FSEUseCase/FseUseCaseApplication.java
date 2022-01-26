a7cb450917f6b296ff1898010907beeea76acd3e
Asvin31 <asvin0903@gmail.com>
2022-01-26 09:26:32 +0530

    inital commit

---
+++ src/main/java/com/FSEUseCase/FseUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.FSEUseCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseUseCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FSEUseCase/FseUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FSEUseCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FseUseCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
